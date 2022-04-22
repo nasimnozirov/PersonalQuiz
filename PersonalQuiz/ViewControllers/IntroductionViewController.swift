a00b82b049e4fdff13d6ccd8255784c40db13dd6
nasim <nasim_nozirov@mail.ru>
2022-04-22 13:20:58 +0300

    push 1

---
+++ PersonalQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  PersonalQuiz
+//
+//  Created by Nasim Nozirov on 16.04.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    @IBAction func unwind(for segue: UIStoryboardSegue) {}
+
+}
+
---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Nasim Nozirov on 21.04.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
